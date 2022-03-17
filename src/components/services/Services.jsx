df21f0de739a9fade2a5403e9f5f5945678e2fb9
Deni Mazzei <denimazzei@Denis-iMac.lan>
2022-03-16 21:20:23 -0500

    services section and css

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,93 @@
 import React from 'react'
 import './services.css'
+import {BsCheckSquare} from 'react-icons/bs'
 
 const Services = () => {
     return (
-        <section id="services">Services</section>
+        <section id="services">
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+
+            <div className="container services_container">
+                <article className="service">
+                    <div className="service_head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+
+                    <ul className="service_list">
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF DESIGN */}
+
+                <article className="service">
+                    <div className="service_head">
+                        <h3>Web Development</h3>
+                    </div>
+
+                    <ul className="service_list">
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF DEVELOPMENT */}
+
+                <article className="service">
+                    <div className="service_head">
+                        <h3>Content Creation</h3>
+                    </div>
+
+                    <ul className="service_list">
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                        <li>
+                            <BsCheckSquare className='service_list-icon' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat maiores nihil debitis consequatur sit itaque, harum dolores ex eveniet corrupti neque ipsam repellat quam perspiciatis autem sequi. Deleniti, illum.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF CONTENT CREATION */}
+            </div>
+        </section>
     )
 }
 

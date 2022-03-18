949481659ca33d5fe9836a84d038e280d7ccee0e
Deni Mazzei <denimazzei@Denis-iMac.lan>
2022-03-17 21:28:55 -0500

    added swiper component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,33 +3,63 @@ import './testimonials.css'
 import CLIENT1 from '../../assets/vader_1.jpeg'
 import CLIENT2 from '../../assets/deni-beach1.jpeg'
 
+// import Swiper core and required modules
+import { Navigation, Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+const data = [
+    {
+        avatar: CLIENT1,
+        name: "Vader",
+        review: "I find your lack of faith disturbing."
+    },
+    {
+        avatar: CLIENT2,
+        name: "Beach",
+        review: "Let's go!"
+    },
+
+]
+
 const Testimonials = () => {
     return (
         <section id="testimonials">
             <h5> Reviews from Clients</h5>
             <h2>Testimonials</h2>
 
-            <div className="container testimonials_container">
-                <article className="testimonial">
-                    <div className='client_avatar'>
-                        <img src={CLIENT1} alt="client"/>
-                    </div>    
-                    <h5 className="client_name">Vader</h5>
-                    <small className="client_review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ducimus quia eos a praesentium maiores, ipsam corrupti impedit quam asperiores labore, reiciendis quo esse eligendi aut ab alias! Rerum, temporibus?
-                    </small>
-                </article>
-                <article className="testimonial">
-                    <div className='client_avatar'>
-                        <img src={CLIENT2} alt="client"/>
-                    </div>    
-                    <h5 className="client_name">Beach</h5>
-                    <small className="client_review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ducimus quia eos a praesentium maiores, ipsam corrupti impedit quam asperiores labore, reiciendis quo esse eligendi aut ab alias! Rerum, temporibus?
-                    </small>
-                </article>
+            <Swiper className="container testimonials_container"
+            // install Swiper modules
+            modules={[Pagination, Navigation]}
+            spaceBetween={40}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            >
+                {
+                    data.map(({avatar, name, review}, index) => {
+                        return(
+                            <SwiperSlide key={index} className="testimonial">
+                            <div className='client_avatar'>
+                                <img src={avatar} alt="client"/>
+                            </div>    
+                            <h5 className="client_name">{name}</h5>
+                            <small className="client_review">{review}
+                            </small>
+                            </SwiperSlide>
+                        )
+                    })
+                }
                 
-            </div>
+               
+                
+            </Swiper>
         </section>
     )
 }

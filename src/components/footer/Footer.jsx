da04417298a1c8ddfd7b55dcba4cc7bd1cf00601
Deni Mazzei <denimazzei@Denis-iMac.lan>
2022-03-18 19:59:36 -0500

    footer and css styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,11 +3,15 @@ import './footer.css'
 import {FaFacebookF} from "react-icons/fa"
 import {FaInstagram} from "react-icons/fa"
 import {FaLinkedin} from "react-icons/fa"
+import LOGO from "../../assets/dm-logo.png"
 
 const Footer = () => {
     return (
         <footer>
-            <a href="#" className='footer_logo'>Deni Mazzei</a>
+            
+            <img src={LOGO} alt='logo' className='footer_logo' href="#"/>
+
+            
 
             <ul className='permalinks'>
                 <li><a href="#">Home</a></li>
@@ -19,15 +23,17 @@ const Footer = () => {
                 <li><a href="#contact">Contact</a></li>
             </ul>
 
+            <div className="footer_copyright">
+                <small>&copy; {new Date().getFullYear()} Deni Mazzei</small>
+            </div>
+
             <div className="footer_socials">
                 <a href="https://facebook.com/denimazzei"><FaFacebookF /></a>
                 <a href="https://instagram.com/denimazzei"><FaInstagram /></a>
                 <a href="https://linkedin.com/deni-mazzei"><FaLinkedin /></a>
             </div>
 
-            <div className="footer_copyright">
-                <small>&copy; {new Date().getFullYear()} Deni Mazzei</small>
-            </div>
+           
         </footer>
     )
 }
